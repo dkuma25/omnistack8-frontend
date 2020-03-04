027a90c748d20d28de98f676bea72be1b61e4aa6
Gabriel Gomes <bibiel2605@gmail.com>
2020-03-04 18:21:37 -0300

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './Login.css'
+import api from '../services/api'
+
+import logo from '../assets/logo.svg'
+
+function Login({ history }) {
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    if (username) {
+      const response = await api.post('/devs', {
+        username
+      })
+      const { _id } = response.data
+      history.push(`/dev/${_id}`)
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev"/>
+        <input 
+          placeholder="Digite seu usuário no Github..."
+          value={username}
+          onChange={e => setUsername(e.target.value)} />
+        <button type="submit">Logar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
